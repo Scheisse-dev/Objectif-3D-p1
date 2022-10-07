5ecc2f4610970f95abbae3247ac8600b65453db3
Scheisse <78857441+Scheisse-dev@users.noreply.github.com>
2022-10-07 16:56:27 +0200

    add comment 1

--- LinkedList/LinkedList/Node.h
+++ LinkedList/LinkedList/Node.h
@@ -3,11 +3,12 @@ template<typename T>
 class Node
 {
 	private:
-		Node* next = nullptr;
+		Node* next = nullptr; //nouveau pointeur
 		Node* prev = nullptr; 
-		T data = T(); 
+		T data = T(); // nouveau template de data = template, donc une valeur sans type
+#pragma region constructor
 	public:
-		Node() = default; 
+		Node() = default;
 		Node(const T& _data, Node* _next = nullptr, Node* _prev = nullptr);
 		Node* Next() const;
 		Node* Prev() const; 
@@ -19,17 +20,21 @@ class Node
 		operator T(); 
 
 };
+#pragma endregion constructor
 
 template<typename T>
-inline Node<T>::Node(const T& _data, Node* _next, Node* _prev)
+inline Node<T>::Node(const T& _data, Node* _next, Node* _prev) // inline permet de réduire l'impact des appels de fonction 
+															   // Node<T> appel de la classe Node avec un template 
+															   //const T& _data déclaration d'une constante data prenant le template comme type 
+															   // Node* _prev, déclaration d'un noveau pointeur pour attribuer les copies
 {
-	next = _next;
+	next = _next; //création de copies 
 	prev = _prev;
 	data = _data;
 }
 
 template<typename T>
-Node<T>* Node<T>::Next() const
+Node<T>* Node<T>::Next() const // Node<T> appel le constructeur de la classe node; 
 {
 	return next;
 };
