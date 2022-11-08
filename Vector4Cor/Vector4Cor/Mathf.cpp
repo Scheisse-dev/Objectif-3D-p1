c0a7a85fd34755664407264382e38e2f43147760
Scheisse <78857441+Scheisse-dev@users.noreply.github.com>
2022-11-08 16:12:39 +0100

    vector4 corr final

---
+++ Vector4Cor/Vector4Cor/Mathf.cpp
@@ -0,0 +1,31 @@
+#include "Mathf.h"
+#include <complex>
+
+
+
+float Mathf::Sqrt(const float _value)
+{
+    return std::sqrt(_value);
+}
+
+float Mathf::Min(const float _a, const float _b)
+{
+    return _a < _b ? _a : _b;
+}
+
+float Mathf::Max(const float _a, const float _b)
+{
+    return _a > _b ? _a : _b;
+}
+
+float Mathf::Abs(const float _value)
+{
+    return std::abs(_value);
+}
+
+float Mathf::Clamp01(const float _value)
+{
+    if (_value < 0) return 0;
+    if (_value > 1) return 1;
+    return _value;
+}
---
+++ Vector4Cor/Vector4Cor/Mathf.h
@@ -0,0 +1,22 @@
+#pragma once
+class Mathf
+{
+#pragma region methods
+public: 
+	template <typename T>
+	static T Clamp(const T& _current, const T& _min, const T& _max);
+	static float Clamp01(const float _value); 
+	static float Sqrt(const float _value);
+	static float Min(const float _a, const float _b);
+	static float Max(const float _a, const float _b);
+	static float Abs(const float _value);
+#pragma endregion methods
+};
+
+template<typename T>
+inline T Mathf::Clamp(const T& _current, const T& _min, const T& _max)
+{
+	if (_current < _min) return _min;
+	if (_current > _max) return _max;
+	return _current; 
+}
