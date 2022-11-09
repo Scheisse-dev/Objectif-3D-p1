3ea702dc869beb07fb846f13451eee4f69aefa8c
Scheisse <78857441+Scheisse-dev@users.noreply.github.com>
2022-11-09 17:00:17 +0100

    add isPointer

---
+++ MemoryExo/MemoryExo/MemoryExo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+template<typename T>
+struct IsPointer { enum { Value = true }; };
+
+template<typename T>
+struct IsPointer<T*> { enum { Value = true }; };
+
+template<typename T>
+struct IsPointer<const T> { enum { Value = IsPointer<T>::Value }; };
+
+
+int main()
+{
+    std::cout << IsPointer<int>::Value << std::endl;
+    std::cout << IsPointer<int*>::Value << std::endl;
+    std::cout << IsPointer<const int*>::Value << std::endl;
+    std::cout << IsPointer<const int>::Value << std::endl;
+}
+
