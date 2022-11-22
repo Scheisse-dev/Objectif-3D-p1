0c0711b91245b3a3d585456e5b3eb4a3d2ace570
Scheisse <78857441+Scheisse-dev@users.noreply.github.com>
2022-11-22 11:17:12 +0100

    save Window

--- WindowExo/WindowExo/WindowExo.cpp
+++ WindowExo/WindowExo/WindowExo.cpp
@@ -1,36 +1,43 @@
-#ifndef UNICODE
-#define UNICODE
-#endif 
-
 #include <windows.h>
+#include <iostream>
 
 
-const wchar_t CLASS_NAME[] = L"Sample Window Class";
+HINSTANCE hInstance = nullptr;
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
-    case WM_SIZE:
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
     {
-        int width = LOWORD(lParam);  // Macro to get the low-order word.
-        int height = HIWORD(lParam); // Macro to get the high-order word.
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        EndPaint(hwnd, &ps);
     }
-    break;
+    return 0;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 
 
-
 int main()
 {
-    WNDCLASS wc = {};
+   
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+    WNDCLASS wc = {0, 0, 0 , 0 , 0, 0 , 0, 0 ,0 , 0};
 
     wc.lpfnWndProc = WindowProc;
-    wc.hInstance = nullptr;
+    wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
+    
+    RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(
         0,                              // Optional window styles.
@@ -38,14 +45,21 @@ int main()
         L"Learn to Program Windows",    // Window text
         WS_OVERLAPPEDWINDOW,            // Window style
 
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, // taille 
 
         NULL,       // Parent window    
         NULL,       // Menu
-        nullptr,      // Instance handle
+        hInstance,  // Instance handle
         NULL        // Additional application data
     );
-    ShowWindow(hwnd, 10);
     
+
+    ShowWindow(hwnd, 10);
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 }
