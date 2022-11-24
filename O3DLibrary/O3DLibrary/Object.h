1ffe488e2160f4289c18f6e73dcb0ed63a63db7c
Scheisse <78857441+Scheisse-dev@users.noreply.github.com>
2022-11-24 16:59:39 +0100

    fail 9

--- O3DLibrary/O3DLibrary/Object.h
+++ O3DLibrary/O3DLibrary/Object.h
@@ -1,32 +1,73 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector> 
+#include <map> 
 #include "O3DLibrary.h"
+
+// fail count = 8
+
+//namespace Core
+//{
+//
+//
+//	namespace PrimitiveType
+//	{
+//		class FString;
+//		class Boolean;
+//	}
+//	template<typename Var>
+//	class O3DLIBRARY_API FieldInfo
+//	{
+//	private:
+//		Core::PrimitiveType::FString name;
+//		Var variable;
+//	public:
+//		FieldInfo(Core::PrimitiveType::FString _name, Var _variable)
+//		{
+//			name = _name;
+//			variable = _variable;
+//			std::map< _name, _variable> map; 
+//		}
+//
+//	};
+//}
 
 namespace Core
 {
+	//template<typename T>
+	//struct FileInfo
+	//{
+	//	PrimitiveType::FString name;
+	//	T value;
+
+	//};
+
+	 
 	namespace PrimitiveType
 	{
 		class FString;
 		class Boolean;
-
 	}
-		class Object
-		{
+	
+	class O3DLIBRARY_API Object
+	{
+
 #pragma region constructor/destructor
-		public:
-			Object() = default;
-			virtual ~Object() = default;
+	public:
+		Object() = default;
+		virtual ~Object() = default;
 #pragma endregion constructor/destructor
 #pragma region methods
-		public:
-			O3DLIBRARY_API virtual Core::PrimitiveType::Boolean Equals(const Object* _obj) const;
-			O3DLIBRARY_API virtual Core::PrimitiveType::FString ToString() const;
+	public:
+		virtual Core::PrimitiveType::Boolean Equals(const Object* _obj) const;
+		virtual Core::PrimitiveType::FString ToString() const;
 
-#pragma region methods 
+#pragma endregion methods 
 
 		};
 		typedef Object* object;
+
 	
 }
 
